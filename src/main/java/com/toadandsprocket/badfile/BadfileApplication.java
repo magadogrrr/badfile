d38afab43a1682b3606be6a8a7c85c9db01eb956
Payseno, Torrence <tpayseno@visa.com>
2020-08-08 03:30:06 -0700

    init commit

---
+++ src/main/java/com/toadandsprocket/badfile/BadfileApplication.java
@@ -0,0 +1,13 @@
+package com.toadandsprocket.badfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toadandsprocket/badfile/BadfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toadandsprocket.badfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
